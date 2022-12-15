df765a33781e3114079ef458a37fb567d486b5a6
Echpochmaker <pasha.bobyliov@yandex.ru>
2022-12-15 16:48:20 +0300

    Adding a project

---
+++ CaesarEncryption/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace CaesarEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelClose = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text = new System.Windows.Forms.TextBox();
+            this.key = new System.Windows.Forms.TextBox();
+            this.LanguageBox = new System.Windows.Forms.ComboBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(140)))), ((int)(((byte)(106)))));
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.labelClose);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(251, 54);
+            this.panel2.TabIndex = 5;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseMove);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(3, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 26);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Шифрование Цезаря ";
+            this.label5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Label5_MouseDown);
+            this.label5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Label5_MouseMove);
+            // 
+            // labelClose
+            // 
+            this.labelClose.AutoSize = true;
+            this.labelClose.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelClose.ForeColor = System.Drawing.Color.White;
+            this.labelClose.Location = new System.Drawing.Point(231, 0);
+            this.labelClose.Name = "labelClose";
+            this.labelClose.Size = new System.Drawing.Size(19, 18);
+            this.labelClose.TabIndex = 0;
+            this.labelClose.Text = "X";
+            this.labelClose.Click += new System.EventHandler(this.LabelClose_Click);
+            this.labelClose.MouseEnter += new System.EventHandler(this.LabelClose_MouseEnter);
+            this.labelClose.MouseLeave += new System.EventHandler(this.LabelClose_MouseLeave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(135)))), ((int)(((byte)(169)))));
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 240);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.text, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.key, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LanguageBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Result, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 54);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(251, 156);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Результат:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Текст:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ключ:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Язык:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(90, 3);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(157, 20);
+            this.text.TabIndex = 11;
+            this.text.MouseEnter += new System.EventHandler(this.Text_MouseEnter);
+            this.text.MouseLeave += new System.EventHandler(this.Text_MouseLeave);
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(90, 29);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(105, 20);
+            this.key.TabIndex = 12;
+            this.key.MouseEnter += new System.EventHandler(this.Key_MouseEnter);
+            this.key.MouseLeave += new System.EventHandler(this.Key_MouseLeave);
+            // 
+            // LanguageBox
+            // 
+            this.LanguageBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LanguageBox.FormattingEnabled = true;
+            this.LanguageBox.Items.AddRange(new object[] {
+            "Русский",
+            "English"});
+            this.LanguageBox.Location = new System.Drawing.Point(90, 55);
+            this.LanguageBox.Name = "LanguageBox";
+            this.LanguageBox.Size = new System.Drawing.Size(105, 21);
+            this.LanguageBox.TabIndex = 13;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(90, 82);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Size = new System.Drawing.Size(157, 67);
+            this.Result.TabIndex = 14;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.ClearButton);
+            this.flowLayoutPanel1.Controls.Add(this.EnterButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 210);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(251, 30);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(106)))), ((int)(((byte)(106)))));
+            this.ClearButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ClearButton.FlatAppearance.BorderSize = 0;
+            this.ClearButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ClearButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearButton.Location = new System.Drawing.Point(180, 3);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(68, 23);
+            this.ClearButton.TabIndex = 14;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(168)))), ((int)(((byte)(113)))));
+            this.EnterButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EnterButton.FlatAppearance.BorderSize = 0;
+            this.EnterButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(168)))), ((int)(((byte)(67)))));
+            this.EnterButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(140)))), ((int)(((byte)(91)))));
+            this.EnterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EnterButton.Location = new System.Drawing.Point(91, 3);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(83, 23);
+            this.EnterButton.TabIndex = 15;
+            this.EnterButton.Text = "Выполнить";
+            this.EnterButton.UseVisualStyleBackColor = false;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 240);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelClose;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.ComboBox LanguageBox;
+        private System.Windows.Forms.TextBox Result;
+    }
+}
+
---
+++ CaesarEncryption/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CaesarEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            key.Text = "Введите ключ";
+            key.ForeColor = Color.Gray;
+        }
+
+        #region Закрытие формы
+        private void LabelClose_Click(object sender, EventArgs e) => Application.Exit();
+        private void LabelClose_MouseEnter(object sender, EventArgs e) => labelClose.ForeColor = Color.Red; 
+        private void LabelClose_MouseLeave(object sender, EventArgs e) => labelClose.ForeColor = Color.White;
+        #endregion
+
+        #region Перемещение формы 
+        Point point;
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+        private void Panel1_MouseDown(object sender, MouseEventArgs e) => point = new Point(e.X, e.Y);
+        private void Panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+        private void Panel2_MouseDown(object sender, MouseEventArgs e) => point = new Point(e.X, e.Y);
+        private void Label5_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) 
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+        private void Label5_MouseDown(object sender, MouseEventArgs e) => point = new Point(e.X, e.Y);
+        #endregion
+
+        #region Подсказки для пользователя
+
+        /// <summary>
+        /// Подсказка "Введите текст"
+        /// </summary>
+        private void Text_MouseEnter(object sender, EventArgs e)
+        {
+            if (text.Text == "Введите текст")
+            {
+                text.Text = " ";
+                text.ForeColor = Color.Black;
+            }
+        }
+
+        /// <summary>
+        /// Подсказка "Введите текст"
+        /// </summary>
+        private void Text_MouseLeave(object sender, EventArgs e)
+        {
+            if (text.Text == " ")
+            {
+                text.Text = "Введите текст";
+                text.ForeColor = Color.Gray;
+            }
+        }
+
+        /// <summary>
+        /// Подсказка "Введите ключ"
+        /// </summary>
+        private void Key_MouseEnter(object sender, EventArgs e)
+        {
+            if (key.Text == "Введите ключ")
+            {
+                key.Text = " ";
+                key.ForeColor = Color.Black;
+            }
+        }
+
+        /// <summary>
+        /// Подсказка "Введите ключ"
+        /// </summary>
+        private void Key_MouseLeave(object sender, EventArgs e)
+        {
+            if (key.Text == " ")
+            {
+                key.Text = "Введите ключ";
+                key.ForeColor = Color.Gray;
+            }
+        }
+        #endregion
+
+        #region Кнопки
+
+        /// <summary>
+        /// Отчистка всех полей
+        /// </summary>>
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            text.Text = " ";
+            key.Text = " ";
+            Result.Text = " ";
+            LanguageBox.SelectedIndex = -1;
+        } 
+
+        /// <summary>
+        /// Выполнение алгоритма
+        /// </summary>   
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string Text = text.Text;
+                int Key = Convert.ToInt32(key.Text);
+                int number;
+                int bias;
+                string res;
+                int j;
+
+                char[] alphabet =
+                {
+                  'а', 'б', 'в', 'г', 'д',
+                  'е', 'ё', 'ж', 'з', 'и',
+                  'й', 'к', 'л', 'м', 'н',
+                  'о', 'п', 'р', 'с', 'т',
+                  'у', 'ф', 'х', 'ц', 'ч',
+                  'ш', 'щ', 'ъ', 'ы', 'ь',
+                  'э', 'ю', 'я'
+                };
+                char[] Alphabet =
+                { 
+                  'А', 'Б', 'В', 'Г', 'Д',
+                  'Е', 'Ё', 'Ж', 'З', 'И',
+                  'Й', 'К', 'Л', 'М', 'Н',
+                  'О', 'П', 'Р', 'С', 'Т',
+                  'У', 'Ф', 'Х', 'Ц', 'Ч',
+                  'Ш', 'Щ', 'Ъ', 'Ы', 'Ь',
+                  'Э', 'Ю', 'Я'
+                };
+
+                char[] english_alphabet =
+                {
+                  'a', 'b', 'c', 'd', 'e',
+                  'f', 'g', 'h', 'i', 'j',
+                  'k', 'l', 'm', 'n', 'o',
+                  'p', 'q', 'r', 's', 't',
+                  'u', 'v', 'w', 'x', 'y',
+                  'z',
+                };
+                char[] English_Alphsbet =
+                {
+                  'A', 'B', 'C', 'D', 'E',
+                  'F', 'G', 'H', 'I', 'J',
+                  'K', 'L', 'M', 'N', 'O',
+                  'P', 'Q', 'R', 'S', 'T',
+                  'U', 'V', 'W', 'X', 'Y',
+                  'Z'
+                };
+
+                char[] txt = Text.ToCharArray();
+                int selected = LanguageBox.SelectedIndex;
+
+                switch (selected)
+                {
+                    case 0:
+                        {                            
+                            if (Key >= 33)
+                                MessageBox.Show("Ключ не может быть больше 32!");
+                            else
+                            {
+                                for (int i = 0; i < txt.Length; i++)
+                                {
+                                    for (j = 0; j < alphabet.Length; j++)
+                                    {
+                                        if (txt[i] == alphabet[j])
+                                            break;
+                                    }
+
+                                    if (j != 33)
+                                    {
+                                        number = j;
+                                        bias = number + Key;
+
+                                        if (bias > 32)
+                                            bias -= 33;
+
+                                        txt[i] = alphabet[bias];
+                                    }
+                                }
+
+                                for (int i = 0; i < txt.Length; i++)
+                                {
+                                    for (j = 0; j < Alphabet.Length; j++)
+                                    {
+                                        if (txt[i] == Alphabet[j])
+                                            break;
+                                    }
+
+                                    if (j != 33)
+                                    {
+                                        number = j;
+                                        bias = number + Key;
+
+                                        if (bias > 32)
+                                            bias -= 33;
+
+                                        txt[i] = Alphabet[bias];
+                                    }
+                                }
+                                res = new string(txt);
+                                Result.Text = Convert.ToString(res);
+                                File.WriteAllText("Result.txt", res);
+                                MessageBox.Show("Результат ещё записан в файл ''Result.txt''", "Результат");
+                            }
+                        }
+                        break;
+
+                    case 1:
+                        {
+                            if (Key >= 26)
+                                MessageBox.Show("Ключ не может быть больше 25!");
+                            else
+                            {
+                                for (int i = 0; i < txt.Length; i++)
+                                {
+                                    for (j = 0; j < english_alphabet.Length; j++)
+                                    {
+                                        if (txt[i] == english_alphabet[j])
+                                            break;
+                                    }
+
+                                    if (j != 26)
+                                    {
+                                        number = j;
+                                        bias = number + Key;
+
+                                        if (bias > 25)
+                                            bias -= 26;
+
+                                        txt[i] = english_alphabet[bias];
+                                    }
+                                }
+
+                                for (int i = 0; i < txt.Length; i++)
+                                {
+                                    for (j = 0; j < English_Alphsbet.Length; j++)
+                                    {
+                                        if (txt[i] == English_Alphsbet[j])
+                                            break;
+                                    }
+
+                                    if (j != 26)
+                                    {
+                                        number = j;
+                                        bias = number + Key;
+
+                                        if (bias > 25)
+                                            bias -= 26;
+
+                                        txt[i] = English_Alphsbet[bias];
+                                    }
+                                }
+                                res = new string(txt);
+                                Result.Text = Convert.ToString(res);
+                                File.WriteAllText("Result.txt", res);
+                                MessageBox.Show("Результат ещё записан в файл ''Result.txt''", "Результат");
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (FormatException) 
+            {
+                MessageBox.Show("Введенно некорректное значение в поле!", "Ошибка");
+            }
+        } 
+        #endregion
+
+        /// <summary>
+        /// Загрузка текста из файла при загрузке формы
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e) => text.Text = File.ReadAllText("Text.txt");
+    }
+}
